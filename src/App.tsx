e635bba84dd6571c1a019900fa7a770a13071d4d
caterpi11ar <daiqin1046@gmail.com>
2025-09-05 00:23:00 +0800

    feat: 集成 SpeedInsights 组件到 App 组件中
    - 将 SpeedInsights 组件添加到 App.tsx 中，以提升性能分析功能
    - 修改 App 组件为函数组件形式，并使用 React Fragment 包裹子组件
    - 移除 main.tsx 中的 SpeedInsights 引用，确保组件结构清晰

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
+import type { FC } from 'react'
+import { SpeedInsights } from '@vercel/speed-insights/react'
 import HomePage from '@/pages/HomePage'
 
-function App() {
-  return <HomePage />
+const App: FC = () => {
+  return (
+    <>
+      <SpeedInsights />
+      <HomePage />
+    </>
+  )
 }
 
 export default App
--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-import { SpeedInsights } from '@vercel/speed-insights/react'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
@@ -6,7 +5,6 @@ import 'virtual:uno.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <SpeedInsights />
     <App />
   </StrictMode>,
 )
